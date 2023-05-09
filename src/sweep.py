78d20197cebe333620adcbf1957bc3d185d3ba79
SeongwooKim <tjddn1818@gmail.com>
2023-05-09 17:21:33 +0000

    [fix] run name initialize

--- src/sweep.py
+++ src/sweep.py
@@ -1,9 +1,10 @@
 import wandb
 import yaml
 from train import train
+from utils import get_result_name
 
 def main():
-    with wandb.init() as run:
+    with wandb.init(name=get_result_name()) as run:
         cfg=wandb.config
         train(cfg)
 
@@ -22,5 +23,5 @@ if __name__=="__main__":
         function=main, 
         entity="ggul_tiger",
         project='KLUE',
-        count=10
+        count=20
     )
